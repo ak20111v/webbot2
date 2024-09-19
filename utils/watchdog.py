c1dc026ccdd310baccd72f30072ee51f5633a07a
ak20111v <90554648+ak20111v@users.noreply.github.com>
2024-09-20 00:55:05 +0300

    Update watchdog.py

--- utils/watchdog.py
+++ utils/watchdog.py
@@ -18,32 +18,32 @@ class Watchdog:
 
     def run(self):
         """start watchdog coroutine every day at 02:00"""
-        self.scheduler.add_job(self.check_end_date, "cron", hour=2, minute=0)
+        self.scheduler.add_job(self.check_end_date, "cron", hour=00, minute=1)
         # self.scheduler.add_job(self.check_end_date, 'interval', seconds=5)
         self.scheduler.start()
         logger.success("[+] Watchdog coroutine created and started successfully")
 
-    async def check_end_date(self):
-        logger.info("[+] Checking for users with end date")
-        notified_users = []
-        user_ids_by_day = {}
-        for days in range(-1, 3):
-            user_ids_by_day[days] = get_user_ids_enddate_n_days(days)
-            for user_id in user_ids_by_day[days]:
-                if user_id not in notified_users:
-                    message_text = self.get_message_text(days)
-                    if days == -1:
-                        await bot.send_message(
-                            user_id,
-                            message_text,
-                            reply_markup=await kb.reply.free_user_kb(user_id=user_id),
-                        )
-                        vpn_config.disconnect_peer(user_id)
-                    else:
-                        await bot.send_message(user_id, message_text)
-                    notified_users.append(user_id)
-                    logger.warning(
-                        f"[+] user {user_id} notified about end date {days} days"
+    async def check_end_date(self): 
+        logger.info("[+] Checking for users with end date") 
+        notified_users = [] 
+        user_ids_by_day = {} 
+        for days in range(-1, 3): 
+            user_ids_by_day[days] = get_user_ids_enddate_n_days(days) 
+            for user_id in user_ids_by_day[days]: 
+                if user_id not in notified_users: 
+                    message_text = self.get_message_text(days) 
+                    if days == 0: 
+                        await bot.send_message( 
+                            user_id, 
+                            message_text, 
+                            reply_markup=await kb.reply.free_user_kb(user_id=user_id), 
+                        ) 
+                        await vpn_config.disconnect_peer(user_id) 
+                    else: 
+                        await bot.send_message(user_id, message_text) 
+                    notified_users.append(user_id) 
+                    logger.warning( 
+                        f"[+] user {user_id} notified about end date {days} days" 
                     )
 
         logger.info("Finished checking for users with end date")
